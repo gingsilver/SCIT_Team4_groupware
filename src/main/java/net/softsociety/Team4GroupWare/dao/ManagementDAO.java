67ba2666f5acd51beec2d68657e9e8e3c9c25f0e
YunhyeKayHong <104616676+YunhyeKayHong@users.noreply.github.com>
2022-10-03 20:28:56 +0900

    221003 근태관리 프론트 자바스크립트 작성

--- src/main/java/net/softsociety/Team4GroupWare/dao/ManagementDAO.java
+++ src/main/java/net/softsociety/Team4GroupWare/dao/ManagementDAO.java
@@ -1,11 +1,14 @@
 package net.softsociety.Team4GroupWare.dao;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 import org.apache.ibatis.annotations.Mapper;
 
 import net.softsociety.Team4GroupWare.domain.Salary;
 import net.softsociety.Team4GroupWare.domain.Tax;
+import net.softsociety.Team4GroupWare.domain.TimeSheet;
+import net.softsociety.Team4GroupWare.domain.Vacation;
 
 @Mapper
 public interface ManagementDAO {
@@ -14,5 +17,17 @@ public interface ManagementDAO {
 
 	//개인의 세금정보 불러오기
 	Tax selectTaxInfo(HashMap<String, String> map);
+
+	//개인의 정시 출퇴근 시간 불러오기(2019.09.12의 형태)
+	TimeSheet selectTimesheetOne(HashMap<String, String> map);
+	
+	//개인의 정시 출퇴근 시간 불러오기(9:00, 18:00의 형태)
+	TimeSheet selectTimesheetOnlyTime(String employee_code);
+
+	//개인의 한 달간 출퇴근 기록 불러오기
+	ArrayList<TimeSheet> selectTimesheetOneMonth(String employee_code);
+
+	//개인의 연차 사용 정보 불러오기
+	Vacation getVacationdays(String employee_code);
 
 }
--- src/main/java/net/softsociety/Team4GroupWare/service/ManagementService.java
+++ src/main/java/net/softsociety/Team4GroupWare/service/ManagementService.java
@@ -1,7 +1,11 @@
 package net.softsociety.Team4GroupWare.service;
 
+import java.util.ArrayList;
+
 import net.softsociety.Team4GroupWare.domain.Salary;
 import net.softsociety.Team4GroupWare.domain.Tax;
+import net.softsociety.Team4GroupWare.domain.TimeSheet;
+import net.softsociety.Team4GroupWare.domain.Vacation;
 
 public interface ManagementService {
 
@@ -19,5 +23,36 @@ public interface ManagementService {
 	 * @return 세금정보
 	 */
 	public Tax selectTaxInfo(String employee_code, String tax_year);
+
+	/**
+	 * 개인의 정해져있는 출퇴근 정시 정보 불러오기
+	 * @param employee_code
+	 * @param time_sheet_code 근로계약서에서 설정한 출퇴근시간
+	 * @return 출퇴근 정시정보(2019.09.12의 형태)
+	 */
+	public TimeSheet selectTimesheetOne(String employee_code, String time_sheet_code);
+	
+	/**
+	 * 개인의 정해져있는 출퇴근 정시 정보 불러오기
+	 * @param employee_code
+	 * @return 풀퇴근 정시정보(9:00, 18:00의 형태)
+	 */
+
+	public TimeSheet selectTimesheetOnlyTime(String employee_code);
+
+	/**
+	 * 개인의 한 달간의 출퇴근 기록 불러오기
+	 * @param employee_code
+	 * @return ArrayList<TimeSheet>
+	 */
+	public ArrayList<TimeSheet> selectTimesheetOneMonth(String employee_code);
+
+	/**
+	 * 개인의 남은 연차정보 불러오기
+	 * @param employee_code
+	 * @return Vacation 개인의 연차정보
+	 */
+	public Vacation getVacationdays(String employee_code);
+
 
 }
